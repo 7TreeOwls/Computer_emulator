cdcc76bb346e354576ea5b08851be9c4c2f5245c
7TreeOwls <olaf.czerwony@gmail.com>
2019-03-13 21:43:24 +0000

    Initial commit, with basic files (empty almost)

---
+++ src/alu.h
@@ -0,0 +1,13 @@
+
+
+class Alu
+{
+    public:
+        Alu();
+        int AND(int a, int b);
+        int OR(int a, int b);
+        int add(int a, int b);
+        int substract(int a, int b);
+        int slt(int a, int b)
+        int NOR(int a, int b);
+}
